96a4887cd3c74861765617f15415c8de5c3a6c54
davidkov111 <kovacsdavid648@outlook.com>
2024-08-25 11:14:01 +0300

    debug = false

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #False #True #False #True
+DEBUG = False #True #False #True
 
 # Hosts
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.now.sh', 'localhost']
